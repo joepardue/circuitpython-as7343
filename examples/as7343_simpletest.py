6abeeff8ca9d950444f3528ee9e8917717a508f8
joepardue <joepardue47@gmail.com>
2025-05-10 17:43:20 -0400

    Clean rebuild of AS7343 CircuitPython driver using Adafruit template

---
+++ examples/as7343_simpletest.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2025 Joe Pardue for self
+#
+# SPDX-License-Identifier: Unlicense
